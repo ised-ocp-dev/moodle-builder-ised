a72d4564092a70ee00c70baa555dc3eaeae53857
Michael Milette <michael.milette@tngconsulting.ca>
2020-03-10 17:19:04 -0400

    MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping

--- php-post-assemble/02-patch-moodle.sh
+++ php-post-assemble/02-patch-moodle.sh
@@ -12,6 +12,7 @@ case "$VERSION" in   # Determine branch based on version of Moodle.
 		git cherry-pick origin/MDL-67554-M37 # OAuth2: Filtering to Parameters Included in a Login Request.
 		git cherry-pick origin/MDL-67802-M37 # Revert "MDL-66598 tool_oauth2: Make account confirmation required by default.
 		git cherry-pick origin/m37-MDL-52810 # MDL-52810 search: Prevent including courses in hidden categories.
+		git cherry-pick origin/MDL-61789-M37 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping
     ;;
     38) # MOODLE 3.8
 		git cherry-pick origin/MDL-66849-M38 # OutputComponents: Remove custom menu title when not specified
@@ -19,6 +20,7 @@ case "$VERSION" in   # Determine branch based on version of Moodle.
 		git cherry-pick origin/MDL-67554-M38 # OAuth2: Filtering to Parameters Included in a Login Request.
 		git cherry-pick origin/MDL-67802-M38 # Revert "MDL-66598 tool_oauth2: Make account confirmation required by default.
 		git cherry-pick origin/m38-MDL-52810 # MDL-52810 search: Prevent including courses in hidden categories.
+		git cherry-pick origin/MDL-61789-M38 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping
     ;;
     39) # MOODLE 3.9 (master)
 		git cherry-pick origin/MDL-66849-master # OutputComponents: Remove custom menu title when not specified
@@ -26,6 +28,7 @@ case "$VERSION" in   # Determine branch based on version of Moodle.
 		git cherry-pick origin/MDL-67554-master # OAuth2: Filtering to Parameters Included in a Login Request.
 		git cherry-pick origin/MDL-67802-master # Revert "MDL-66598 tool_oauth2: Make account confirmation required by default.
 		git cherry-pick origin/master-MDL-52810 # MDL-52810 search: Prevent including courses in hidden categories.
+		git cherry-pick origin/MDL-61789-master  # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping
     ;;
     *) # Unknown branch
 		echo "Unknown branch. Export the VERSION variable first. Exiting without patching..."
