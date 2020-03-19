a64c3d9d2c938640b76ef5070702c0d80196a13d
Michael Milette <michael.milette@tngconsulting.ca>
2020-03-19 16:42:00 -0400

    Added patch MDL-61750 - Add support for filtering custom menus.

--- php-post-assemble/02-patch-moodle.sh
+++ php-post-assemble/02-patch-moodle.sh
@@ -7,31 +7,34 @@
 echo "Applying patches to ISED's Moodle."
 case "$VERSION" in   # Determine branch based on version of Moodle.
     37) # MOODLE 3.7
-		git cherry-pick origin/MDL-66849-M37 # OutputComponents: Remove custom menu title when not specified
+		git cherry-pick origin/MDL-66849-M37 # OutputComponents: Remove custom menu title when not specified.
 		git cherry-pick origin/MDL-66856-M37 # Output components: Enable HTML in custom menu.
 		git cherry-pick origin/MDL-67554-M37 # OAuth2: Filtering to Parameters Included in a Login Request.
 		git cherry-pick origin/MDL-67802-M37 # Revert "MDL-66598 tool_oauth2: Make account confirmation required by default.
 		git cherry-pick origin/m37-MDL-52810 # MDL-52810 search: Prevent including courses in hidden categories.
-		#NOT-READY git cherry-pick origin/MDL-61789-M37 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping
-		git cherry-pick origin/MDL-61880-M37 # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely
+		#NOT-READY git cherry-pick origin/MDL-61789-M37 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping.
+		git cherry-pick origin/MDL-61880-M37 # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely.
+		git cherry-pick origin/MDL-61750-M37 # Add support for filtering in Moodle Custom Menus.
     ;;
     38) # MOODLE 3.8
-		git cherry-pick origin/MDL-66849-M38 # OutputComponents: Remove custom menu title when not specified
+		git cherry-pick origin/MDL-66849-M38 # OutputComponents: Remove custom menu title when not specified.
 		git cherry-pick origin/MDL-66856-M38 # Output components: Enable HTML in custom menu.
 		git cherry-pick origin/MDL-67554-M38 # OAuth2: Filtering to Parameters Included in a Login Request.
 		git cherry-pick origin/MDL-67802-M38 # Revert "MDL-66598 tool_oauth2: Make account confirmation required by default.
 		git cherry-pick origin/m38-MDL-52810 # MDL-52810 search: Prevent including courses in hidden categories.
-		#NOT-READY git cherry-pick origin/MDL-61789-M38 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping
-		git cherry-pick origin/MDL-61880-M38 # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely
+		#NOT-READY git cherry-pick origin/MDL-61789-M38 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping.
+		git cherry-pick origin/MDL-61880-M38 # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely.
+		git cherry-pick origin/MDL-61750-M38 # Add support for filtering in Moodle Custom Menus.
     ;;
     39) # MOODLE 3.9 (master)
-		git cherry-pick origin/MDL-66849-master # OutputComponents: Remove custom menu title when not specified
+		git cherry-pick origin/MDL-66849-master # OutputComponents: Remove custom menu title when not specified.
 		git cherry-pick origin/MDL-66856-master # Output components: Enable HTML in custom menu.
 		git cherry-pick origin/MDL-67554-master # OAuth2: Filtering to Parameters Included in a Login Request.
 		git cherry-pick origin/MDL-67802-master # Revert "MDL-66598 tool_oauth2: Make account confirmation required by default.
 		git cherry-pick origin/master-MDL-52810 # MDL-52810 search: Prevent including courses in hidden categories.
-		#NOT-READY git cherry-pick origin/MDL-61789-master # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping
-		git cherry-pick origin/MDL-61880-master # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely
+		#NOT-READY git cherry-pick origin/MDL-61789-master # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping.
+		git cherry-pick origin/MDL-61880-master # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely.
+		git cherry-pick origin/MDL-61750-master # Add support for filtering in Moodle Custom Menus.
     ;;
     *) # Unknown branch
 		echo "Unknown branch. Export the VERSION variable first. Exiting without patching..."
