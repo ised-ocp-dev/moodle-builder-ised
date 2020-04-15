9fddb79fe6e0c606e9be317eac93004a534d42d7
Michael Milette <michael.milette@tngconsulting.ca>
2020-04-15 00:08:49 -0400

    Renamed branch MDL-61750 to MDL-63219.

--- php-post-assemble/02-patch-moodle.sh
+++ php-post-assemble/02-patch-moodle.sh
@@ -14,7 +14,7 @@ case "$VERSION" in   # Determine branch based on version of Moodle.
 		git cherry-pick origin/MDL-52810-M37 # m37-MDL-52810 search: Prevent including courses in hidden categories.
 		#NOT-READY git cherry-pick origin/MDL-61789-M37 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping.
 		git cherry-pick origin/MDL-61880-M37 # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely.
-		git cherry-pick origin/MDL-61750-M37 # Add support for filtering in Moodle Custom Menus.
+		git cherry-pick origin/MDL-63219-M37 # Add support for filtering in Moodle Custom Menus.
 		git cherry-pick origin/MDL-68257-M37 # MDL-68257 book: Fix for overlapping text in book table-of-contents.
     ;;
     38) # MOODLE 3.8
@@ -25,7 +25,7 @@ case "$VERSION" in   # Determine branch based on version of Moodle.
 		git cherry-pick origin/MDL-52810-M38 # m38-MDL-52810 search: Prevent including courses in hidden categories.
 		#NOT-READY git cherry-pick origin/MDL-61789-M38 # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping.
 		git cherry-pick origin/MDL-61880-M38 # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely.
-		git cherry-pick origin/MDL-61750-M38 # Add support for filtering in Moodle Custom Menus.
+		git cherry-pick origin/MDL-63219-M38 # Add support for filtering in Moodle Custom Menus.
 		git cherry-pick origin/MDL-68257-M38 # MDL-68257 book: Fix for overlapping text in book table-of-contents.
     ;;
     39) # MOODLE 3.9 (master)
@@ -36,7 +36,7 @@ case "$VERSION" in   # Determine branch based on version of Moodle.
 		git cherry-pick origin/MDL-52810-master # master-MDL-52810 search: Prevent including courses in hidden categories.
 		#NOT-READY git cherry-pick origin/MDL-61789-master # MDL-61789 auth_oauth2: Allow admin to choose profile fields for mapping.
 		git cherry-pick origin/MDL-61880-master # MDL-61880 auth_oauth2: User field mappings, endpoints updated separetely.
-		git cherry-pick origin/MDL-61750-master # Add support for filtering in Moodle Custom Menus.
+		git cherry-pick origin/MDL-63219-master # Add support for filtering in Moodle Custom Menus.
 		git cherry-pick origin/MDL-68257-master # MDL-68257 book: Fix for overlapping text in book table-of-contents.
     ;;
     *) # Unknown branch
