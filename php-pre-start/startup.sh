7a466346bb33e9e09d56ff11e70524a8c4c852f5
Michael Milette <michael.milette@tngconsulting.ca>
2020-10-14 12:57:26 -0400

    Added robots.txt for non-production sites.

--- php-pre-start/startup.sh
+++ php-pre-start/startup.sh
@@ -6,3 +6,7 @@
 
 # Set-up error page in .htaccess.
 echo "ErrorDocument 404 ${MOODLE_URL}/theme/gcweb/layout/404.php">>.htaccess
+# If URL is on dev-openshift, add robot.txt to discourage search engine crawlers.
+if [[ $MOODLE_URL =~ ".dev.openshift." ]]; then
+  cp php-pre-start/robots.txt .
+fi
