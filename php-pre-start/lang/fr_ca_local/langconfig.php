697c8e888f522aa83621cbd8de36317e0ea325a6
Michael Milette <michael.milette@tngconsulting.ca>
2021-03-02 13:54:56 -0500

    Added customized langauage strings.

---
+++ php-pre-start/lang/fr_ca_local/langconfig.php
@@ -0,0 +1,36 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from https://learning-apprentissage.apps.dev.ocp-dev.ised-isde.canada.ca
+ *
+ * @package    core
+ * @subpackage langconfig
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['strftimedatetime'] = '%d %B %Y, %H h %M';
+$string['strftimedatetimeshort'] = '%Y-%m-%d, %H h %M';
+$string['strftimedaydatetime'] = '%A, %d %B %Y, %H h %M';
+$string['strftimedaytime'] = '%a, %H h %M';
+$string['strftimerecent'] = '%d %b, %H h %M';
+$string['strftimerecentfull'] = '%a, %d %b %Y, %H h %M';
+$string['strftimetime'] = '%H h %M';
+$string['strftimetime12'] = '%H h %M';
+$string['strftimetime24'] = '%H h %M';
